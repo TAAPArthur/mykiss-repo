61a4526509634acaeaca08d5ba6dec35185ba19d
Arthur Williams <taaparthur@gmail.com>
2020-11-07 00:09:14 +0000

    Removed udev and added support for rc.local
    rc.local will be invoked on statup
    udev service has been removed and ndev is invoked through rc.local
    Some system defaults have been updated

--- personal/myrootsetup/files/defaults.sh
+++ personal/myrootsetup/files/defaults.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export KISS_PATH="/etc/repo/repo/core:/etc/repo/repo/extra:/etc/repo/repo/xorg:/etc/repo/community/community"
+export KISS_PATH_ORIGINAL="/etc/repo/repo/core:/etc/repo/repo/extra:/etc/repo/repo/xorg:/etc/repo/community/community"
+export KISS_PATH=$KISS_PATH_ORIGINAL
 export MAKEFLAGS="-j 6"
 export EDITOR=vim
