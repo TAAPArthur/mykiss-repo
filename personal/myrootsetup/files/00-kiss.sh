e23a20217c646c8d67395150eedce6b021f8d254
Arthur Williams <taaparthur@gmail.com>
2021-05-28 01:30:04 -0500

    [myrootsetup] Update
    Allow sys group to own leds
    Set hwclock after writing seed
    Removed kbdrate

--- personal/myrootsetup/files/00-kiss.sh
+++ personal/myrootsetup/files/00-kiss.sh
@@ -5,9 +5,9 @@ export KISS_PATH=$MY_REPO_DIR/personal:$MY_REPO_DIR/forks:$MY_REPO_DIR/prebuilt:
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/extra:$MY_REPO_DIR/python
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/fallback
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/meta_packages
-KISS_PATH=$KISS_PATH:$MY_REPO_DIR/nonfree
+KISS_PATH=$KISS_PATH:$REPO_DIR/repo-bin/bin
 KISS_PATH=$KISS_PATH:$REPO_DIR/repo/core:$REPO_DIR/repo/extra:$REPO_DIR/repo/xorg:$REPO_DIR/community/community
-KISS_PATH=$KISS_PATH:$REPO_DIR/kiss-all/audio
+KISS_PATH=$KISS_PATH:$MY_REPO_DIR/nonfree
 export KISS_PATH_ORIGINAL=$KISS_PATH
 
 export KISS_TMPDIR=/tmp/kiss
