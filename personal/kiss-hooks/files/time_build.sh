26991e50a42f7007308cdc1e3e7b26fbecb9f5d6
Arthur Williams <taaparthur@gmail.com>
2021-08-12 23:31:07 -0500

    kiss-hooks: Reset time after every build

--- personal/kiss-hooks/files/time_build.sh
+++ personal/kiss-hooks/files/time_build.sh
@@ -7,20 +7,22 @@ case $TYPE in
 
    post-build)
        IFS=. read -r _end _ < /proc/uptime
+       (
+       unset u
+       s=$((_end - _start))
+       h=$((s / 60 / 60 % 24))
+       m=$((s / 60 % 60))
 
-       _s=$((_end - _start))
-       _h=$((_s / 60 / 60 % 24))
-       _m=$((_s / 60 % 60))
+       [ "$h" = 0 ] || u="$u${h}h "
+       [ "$m" = 0 ] || u="$u${m}m "
 
-       [ "$_h" = 0 ] || _u="${_u}${_h}h "
-       [ "$_m" = 0 ] || _u="${_u}${_m}m "
-
-       log "$PKG" "Build finished in ${_u:-${_s}s}"
-       if [ "$_m" -gt 1 ] || [ "$_h" -ne 0 ]; then
-           logger -t kiss -i "Long build for $PKG: ${_s}s"
+       log "$PKG" "Build finished in ${u:-${s}s}"
+       if [ "$s" -gt 240 ]; then
+           logger -t kiss -i "Long build for $PKG: ${s}s"
            mkdir -p ~/.local/share/kiss/
-           echo "${_s}s $PKG" >> ~/.local/share/kiss/long_build.log
+           echo "${s}s $PKG" >> ~/.local/share/kiss/long_build.log
        fi
+       )
    ;;
 esac
 
