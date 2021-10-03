6617464205bfa74b23676ce7040f93078badb21b
Arthur Williams <taaparthur@gmail.com>
2021-10-03 04:36:06 -0500

    fixup! Moved perl and python files to community

--- personal/myrootsetup/files/00-kiss.sh
+++ personal/myrootsetup/files/00-kiss.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 REPO_DIR=/etc/repo
-MY_REPO_DIR=$REPO_DIR/mykiss-repo
-export KISS_PATH=$MY_REPO_DIR/personal:$MY_REPO_DIR/forks:$MY_REPO_DIR/prebuilt:$MY_REPO_DIR/ban
-KISS_PATH=$KISS_PATH:$MY_REPO_DIR/extra:$MY_REPO_DIR/python
+MY_REPO_DIR=${MY_REPO_DIR:-$REPO_DIR/mykiss-repo}
+export KISS_PATH=$MY_REPO_DIR/personal:$MY_REPO_DIR/forks:$MY_REPO_DIR/prebuilt
+KISS_PATH=$KISS_PATH:$MY_REPO_DIR/community:$MY_REPO_DIR/extra
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/fallback
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/meta_packages
 KISS_PATH=$KISS_PATH:$REPO_DIR/repo/core:$REPO_DIR/repo/extra:$REPO_DIR/repo/xorg:$REPO_DIR/community/community
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/nonfree
-export KISS_PATH_ORIGINAL=$KISS_PATH
 
 export KISS_TMPDIR=/tmp/kiss
 export KISS_DEPEND_HOOK="sed s/libjpeg-turbo/libjpeg/"
