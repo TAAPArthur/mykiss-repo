3b83e91392cada2f70f9811f6eaa36e0a9d3c327
Arthur Williams <taaparthur@gmail.com>
2021-10-11 14:00:46 -0500

    kiss-hook: update to work with new kiss version

--- personal/kiss-hooks/files/set_compiler.sh
+++ personal/kiss-hooks/files/set_compiler.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 KISS_ALT_CC_DIR=${KISS_ALT_CC_DIR:-/usr/share/kiss-hooks}
-if [ -z "$KISS_DISABLE_COMPILER_CHECK" ] && grep -Rq "^$PKG\$" "$KISS_ALT_CC_DIR"; then
-    case $TYPE in
+if [ -z "$KISS_DISABLE_COMPILER_CHECK" ] && grep -Rq "^$2\$" "$KISS_ALT_CC_DIR"; then
+    case "$1" in
        pre-build) export CC=gcc ;;
        post-build) export CC=${DEFAULT_CC:-cc} ;;
     esac
