cbc41b437ad1e73cf4340583058115ea01478d70
Arthur Williams <taaparthur@gmail.com>
2021-10-17 00:47:00 -0500

    myrootsetup: put community after xorg in KISS_PATH

--- personal/myrootsetup/files/00-kiss.sh
+++ personal/myrootsetup/files/00-kiss.sh
@@ -5,8 +5,8 @@ export KISS_PATH=$MY_REPO_DIR/personal:$MY_REPO_DIR/forks:$MY_REPO_DIR/prebuilt
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/community:$MY_REPO_DIR/extra
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/fallback
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/meta_packages
-KISS_PATH=$KISS_PATH:$REPO_DIR/repo/core:$REPO_DIR/repo/extra:$REPO_DIR/community/community
 KISS_PATH=$KISS_PATH:$REPO_DIR/kiss-xorg/xorg:$REPO_DIR/kiss-xorg/extra:$REPO_DIR/kiss-xorg/community
+KISS_PATH=$KISS_PATH:$REPO_DIR/repo/core:$REPO_DIR/repo/extra:$REPO_DIR/community/community
 KISS_PATH=$KISS_PATH:$MY_REPO_DIR/nonfree
 
 export KISS_TMPDIR=/tmp/kiss
